c1c3f04221dbbb4328c63af9dd5a50c0d62ec63f
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2025-01-15 17:14:40 +0500

    crypto module

--- trading_bot_nestjs/src/app.module.ts
+++ trading_bot_nestjs/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { BinanceModule } from "./binance/binance.module";
 import { WebsocketModule } from "./websocket/websocket.module";
 import { ConfigModule } from "@nestjs/config";
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BinanceModule, WebsocketModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BinanceModule, WebsocketModule, CryptoModule],
   controllers: [],
   providers: [],
 })
