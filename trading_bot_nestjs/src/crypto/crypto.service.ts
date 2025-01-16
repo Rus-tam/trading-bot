5d510b6e1c59cd83a21599d5814303b8a7aac9c7
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2025-01-16 15:35:23 +0500

    CryptoService & IAccountRequestParams

--- trading_bot_nestjs/src/crypto/crypto.service.ts
+++ trading_bot_nestjs/src/crypto/crypto.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import * as crypto from 'node:crypto';
+import { IAccountRequestParams } from '@types';
+
 
 @Injectable()
-export class CryptoService {}
+export class CryptoService {
+        static accountInformation(params: IAccountRequestParams): string {
+            const paramsToSign = `apiKey=${params.apiKey}&timestamp=${params.timestamp}`;
+            const hmac = crypto.createHmac('sha256', params.secretKey);
+            hmac.update(paramsToSign);
+            const signature = hmac.digest('hex');
+
+            return signature;
+    }
+}
---
+++ trading_bot_nestjs/src/types/crypto_types/IAccountRequestParams.ts
@@ -0,0 +1,5 @@
+export interface IAccountRequestParams {
+    apiKey: string;
+    secretKey: string;
+    timestamp: number;
+}
